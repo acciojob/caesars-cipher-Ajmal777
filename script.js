fb743aaf4489cd0f8d1170c43d54cf36a839eadb
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-06-09 18:14:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -38,7 +38,8 @@ function rot13(encodedStr) {
 	  if(encodedStr[i] === ' ') decodedArr.push(' ');
 	  else decodedArr.push(lookup[encodedStr[i]]);
   }
-  return decodedArr; //return decodedArr
+	const res = decodedArr.join('');
+  return res; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
