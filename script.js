dac83f01bdcbd508de825262e1c2d45ffef97547
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-06-09 17:42:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,8 +34,10 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
-  return; //return decodedArr
+  for(let char in encodedStr){
+	  decodedArr.push(lookup[char]);
+  }
+  return decodedArr; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
